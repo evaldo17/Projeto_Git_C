1926aabdb09bb5f94ee715cc3f1b4ea05620c0ea
evaldo <evaldo1706@gmail.com.br>
2021-11-19 11:14:36 -0300

    Segunda versao

---
+++ Exercicio_01.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+    int x,y;
+    printf("Digite um valor para x:");
+    scanf("%d",&x);
+    printf("Digite um valor para y:");
+    scanf("%d",&y);
+    printf("\nA soma de %d + %d = %d\n",x,y,x+y);
+    return 0;
+}
